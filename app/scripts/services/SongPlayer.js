1c1b275941996114380276d7f2841bad3dd58a24
Edward Choi <edwardchoi.2011@gmail.com>
2017-07-26 16:18:04 -0700

    CP7 services pt 2 assignment work

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,7 +2,16 @@
     function SongPlayer() {
         var SongPlayer = {};
 
+        /**
+        * @desc current buzz object audio file
+        * @type {Object}
+        */
         var currentSong = null;
+
+        /**
+        * @desc Buzz object audio file
+        * @type {Object}
+        */
         var currentBuzzObject = null;
 
         /**
@@ -24,18 +33,40 @@
             currentSong = song;
         };
 
+
+        /**
+        * @function playSong
+        * @desc plays the current Buzz object and sets it's property to true
+        * @type {Object} song
+        */
+        var playSong = function(song){
+        	currentBuzzObject.play();
+        	song.playing = true;
+        };
+
+        /**
+        * @function SongPLayer.play
+        * @desc if the current song is not equal to the Buzz ojject song, 
+        * then a new song will be loaded and start playing. If the current song
+        * is the same and if it is paused, then the song will play.
+        * @type {Object} song
+        */
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
-                    currentBuzzObject.play();
+                    playSong(song);
                 }
             }
         };
 
+        /**
+        *f @function SongPlayer.pause
+        * @desc pauses current Buzz object song and sets its properties to false
+        * @type {Object} song
+        */
         SongPlayer.pause = function(song) {
             currentBuzzObject.pause();
             song.playing = false;
