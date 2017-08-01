7411ec4fb21fe467f5f62f7215f2bbb741af9f77
Edward Choi <edwardchoi.2011@gmail.com>
2017-08-01 15:13:36 -0700

    CP8 assignment error

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -18,8 +18,7 @@
         */
         var setSong = function(song) {
             if (currentBuzzObject) {
-                currentBuzzObject.stop();
-                SongPlayer.currentSong.playing = null;
+                stopSong();
             }
 
             currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -30,7 +29,6 @@
             SongPlayer.currentSong = song;
         };
 
-
         /**
         * @function playSong
         * @desc plays the current Buzz object and sets it's property to true
@@ -40,6 +38,26 @@
         	currentBuzzObject.play();
         	song.playing = true;
         };
+
+        /**
+        * @function pauseSong
+        * @desc pauses song
+        * @type {Object} song
+        */
+        var pauseSong = function(song){
+            currentBuzzObject.pause();
+            song.playing = false;
+        }
+
+        /**
+        * @function stopSong
+        * @desc stops song
+        * @type {Object} song
+        */
+        var stopSong = function() {
+            currentBuzzObject.stop();
+            SongPlayer.currentSong.playing = null;
+        }
 
         /**
         * @function getSongIndex
@@ -83,7 +101,7 @@
         SongPlayer.pause = function(song) {
             song = song || SongPlayer.currentSong;
             currentBuzzObject.pause();
-            song.playing = false;
+            pauseSong(song);
         };
 
         /**
@@ -96,14 +114,31 @@
 		    currentSongIndex--;
 
 		    if (currentSongIndex < 0) {
-	        currentBuzzObject.stop();
-	        SongPlayer.currentSong.playing = null;
+	            stopSong();
 	   	    } else {
-	        var song = currentAlbum.songs[currentSongIndex];
-	        setSong(song);
-	        playSong(song);
+    	        var song = currentAlbum.songs[currentSongIndex];
+    	        setSong(song);
+    	        playSong(song);
 	    	}
 		};
+
+        /**
+        *f @function SongPlayer.next
+        * @desc gets next song index and stops playing if click next on last song in album
+        * @type {Object}
+        */
+        SongPlayer.next = function(){
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex++;
+
+            if (currentSongIndex > Object.keys(currentAlbum).length) {
+                stopSong();
+            } else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
+            }
+        }
 
         return SongPlayer;
     }
